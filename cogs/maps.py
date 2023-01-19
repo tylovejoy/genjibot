b33f8ce17b41aa84e09870de9846f73d56bb02bd
Ty Lovejoy <ty@fromskytocenter.com>
2023-01-18 21:23:44 -0600

    difficulty map search fix

--- cogs/maps.py
+++ cogs/maps.py
@@ -150,7 +150,7 @@ class Maps(commands.Cog):
         embed.set_thumbnail(url=None)
         maps: list[database.DotRecord | None] = []
 
-        ranges = utils.DIFFICULTIES_RANGES.get(difficulty, None)
+        ranges = utils.TOP_DIFFICULTIES_RANGES.get(difficulty.value, None)
         low_range = None if ranges is None else ranges[0]
         high_range = None if ranges is None else ranges[1]
         async for _map in itx.client.database.get(
@@ -193,10 +193,11 @@ class Maps(commands.Cog):
                 ($2::text IS NULL OR map_type LIKE $2) AND
                 ($3::text IS NULL OR map_name = $3) AND
                 ($4::text IS NULL OR mechanics LIKE $4) AND
-                ($5::numeric(10, 2) IS NULL OR difficulty >= $5) AND
-                ($6::numeric(10, 2) IS NULL OR difficulty < $6) AND
+                (($5::numeric(10, 2) IS NULL OR difficulty >= $5) AND
+                ($6::numeric(10, 2) IS NULL OR difficulty < $6)) AND
                 ($7::int IS NULL OR quality >= $7) AND
-                ($8::bigint IS NULL OR $8 = ANY(creator_ids));
+                ($8::bigint IS NULL OR $8 = ANY(creator_ids))
+                ORDER BY difficulty, quality;
             """,
             map_code,
             map_type,
