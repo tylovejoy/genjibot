675152a4a99fe8f6d52878ab9f1b220444f78210
Ty Lovejoy <ty@fromskytocenter.com>
2023-02-10 10:35:13 -0600

    fix: make name change command ephemeral

--- cogs/personal.py
+++ cogs/personal.py
@@ -28,7 +28,8 @@ class Personal(commands.Cog):
             nickname: New nickname
         """
         await itx.response.send_message(
-            f"Changing your nick name from {itx.client.all_users[itx.user.id]['nickname']} to {nickname}"
+            f"Changing your nick name from {itx.client.all_users[itx.user.id]['nickname']} to {nickname}",
+            ephemeral=True,
         )
         await itx.client.database.set(
             "UPDATE users SET nickname=$2 WHERE user_id=$1",
