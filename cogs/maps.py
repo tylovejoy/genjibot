01745a636738b073ba3aa7625a467d73beac81da
Ty Lovejoy <48333668+tylovejoy@users.noreply.github.com>
2023-05-01 09:24:24 -0500

    fix(map-search): playtest maps with no votes show as None - None in search

--- cogs/maps.py
+++ cogs/maps.py
@@ -183,8 +183,8 @@ class Maps(commands.Cog):
                  END AS required_votes, playtest.value, map_code FROM playtest WHERE is_author = True),
                
                playtest_avgs AS 
-               (SELECT p.map_code, COUNT(p.value) as "count", required_votes 
-               FROM playtest p LEFT JOIN required rv ON p.map_code=rv.map_code WHERE is_author = FALSE GROUP BY p.map_code, required_votes),
+               (SELECT p.map_code, COUNT(p.value) - 1 as "count", required_votes 
+               FROM playtest p LEFT JOIN required rv ON p.map_code=rv.map_code GROUP BY p.map_code, required_votes),
                
                
                
