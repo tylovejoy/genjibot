2716e18806d7e44e46a35a6e5b470d0e29c0c8dd
Ty Lovejoy <ty@fromskytocenter.com>
2024-12-15 16:48:12 -0600

    style: format

--- main.py
+++ main.py
@@ -51,6 +51,7 @@ def setup_logging() -> None:
             hdlr.close()
             log.removeHandler(hdlr)
 
+
 rabbitmq_user = os.getenv("RABBITMQ_DEFAULT_USER")
 rabbitmq_pass = os.getenv("RABBITMQ_DEFAULT_PASS")
 
