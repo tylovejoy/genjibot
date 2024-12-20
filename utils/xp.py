576ed9b298ac5767d78dc2d821356b5076ab287b
Ty Lovejoy <ty@fromskytocenter.com>
2024-12-20 11:31:57 -0600

    feat: initial xp

--- utils/xp.py
+++ utils/xp.py
@@ -1,28 +1,37 @@
 from __future__ import annotations
 
+import logging
 from functools import wraps
-from typing import TYPE_CHECKING, Callable, ParamSpec, TypeVar
+from typing import TYPE_CHECKING, Awaitable, Callable, ParamSpec, TypeVar
 
+import msgspec
 from discord.utils import maybe_coroutine
 
 if TYPE_CHECKING:
     from core import Genji
     from database import Database
 
-P = ParamSpec("P")
-R = TypeVar("R")
+    P = ParamSpec("P")
+    R = TypeVar("R")
 
-def safe_execution(func: Callable[P, R]) -> Callable[P, bool]:
+
+log = logging.getLogger(__name__)
+
+
+def safe_execution(func: Callable[P, R]) -> Callable[P, Awaitable[bool]]:
     """Wrap a function to return a bool on its success."""
+
     @wraps(func)
-    def wrapper(*args: P.args, **kwargs: P.kwargs) -> bool:
+    async def wrapper(*args: P.args, **kwargs: P.kwargs) -> bool:
         try:
-            _ = maybe_coroutine(func, *args, **kwargs)
+            await maybe_coroutine(func, *args, **kwargs)
             return True
-        except Exception as e:
-            print(f"An error occurred: {e}")
+        except Exception as _:
+            log.error("An error occurred during the execution of %s:", func.__name__, exc_info=True)
             return False
+
     return wrapper
+
 
 class XP:
     def __init__(self, bot: Genji) -> None:
@@ -34,9 +43,13 @@ class XP:
         query = """
             INSERT INTO xptable (user_id, amount) VALUES ($1, $2)
             ON CONFLICT (user_id) DO UPDATE
-            SET amount = amount + excluded.amount
+            SET amount = xptable.amount + excluded.amount
         """
         await self._db.execute(query, user_id, amount)
 
-    async def _(self):
-        self.grant_user_xp(141372217677053952, 1)
+    async def _(self, user_id: int) -> None:
+        success = await self.grant_user_xp(user_id, 1)
+        if success:
+            log.info("Successfully granted xp for user: %s", "nebula")
+        else:
+            log.info("Failed to grant xp for user: %s", "nebula")
